36f2ca7bdfa82e92922194c4b512a0198f26ed23
KroegerLeif <Leif.kr@outlook.com>
2025-08-25 12:48:46 +0200

    Complition of Medium Task TDD

--- src/main/java/org/example/TDDTask.java
+++ src/main/java/org/example/TDDTask.java
@@ -10,4 +10,30 @@ public class TDDTask {
     public static char getFirstCharacter(String text){
        return text.charAt(0);
     }
+    public static String reverseString(String text){
+        return new StringBuilder(text).reverse().toString();
+    }
+    public static boolean isPalindrome(String text){
+        return text.toLowerCase().equals(reverseString(text).toLowerCase());
+    }
+    public static int countVowels(String text)
+    {
+        int count = 0;
+        for(int i = 0; i < text.length(); i++)
+        {
+            switch(text.charAt(i)){
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    count++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return count;
+    }
+
 }
--- src/test/java/TDDTaksTest.java
+++ src/test/java/TDDTaksTest.java
@@ -37,4 +37,48 @@ public class TDDTaksTest {
         //Then
         assertEquals(expected,actual);
     }
+
+    @Test
+    void reverseString_returnReversedInput_TextGiven(){
+        //Given
+        String text = "Java";
+        String expected = "avaJ";
+        //When
+        String actual = TDDTask.reverseString(text);
+        //Then
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void isPalindrome_returnTrue_givenPalidrom(){
+        //Given
+        String text = "Otto";
+        boolean expected = true;
+        //When
+        boolean actual = TDDTask.isPalindrome(text);
+        //Then
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void isPalindrome_returnFalse_givenNoPalidrom(){
+        //Given
+        String text = "Java";
+        boolean expected = false;
+        //When
+        boolean actual = TDDTask.isPalindrome(text);
+        //Then
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void countVowels_returnVowelsAmount_TextWithVowels(){
+        //Given
+        String text = "Hallo";
+        int expected = 2;
+        //When
+        int actual = TDDTask.countVowels(text);
+        //Then
+        assertEquals(expected,actual);
+    }
 }
