98e44779d70d9fc4150aa3cc65dc513689b5fce1
KroegerLeif <Leif.kr@outlook.com>
2025-08-25 15:16:07 +0200

    merge problem solved

--- src/main/java/org/example/javaBasics/Expert_RomanNumber.java
+++ src/main/java/org/example/javaBasics/Expert_RomanNumber.java
@@ -1,66 +1,42 @@
 package org.example.javaBasics;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Expert_RomanNumber
 {
     public static void main(String[] args) {
-        int arabicNumber = 75;
-        String romanNumber = "";
-        int[] highestAndLowerNumber = highestAndLowerNumber(arabicNumber);
-        // System.out.println(highestAndLowerNumber[0]); -50
-        romanNumber += convertToRomanNumber(highestAndLowerNumber[0]);
-        //System.out.println(highestAndLowerNumber[1]); -10
-        arabicNumber -= highestAndLowerNumber[0];
-
-        int rest = getRestNummber(highestAndLowerNumber[1], arabicNumber);
-        int wiederholungen = arabicNumber  / highestAndLowerNumber[1];
-        if(wiederholungen <4){
-            for(int i = 0 ; i < wiederholungen ; i++){
-                romanNumber += convertToRomanNumber(highestAndLowerNumber[1]);
-            }
-        }else{
-            System.out.println("sonderfall");
-        }
+        int arabicNumber = 99;
+        String romanNumber = convertToRoman(arabicNumber);
 
         System.out.println(romanNumber);
-        System.out.println(rest);
 
 
     }
-    private static int[] highestAndLowerNumber(int x){
-        int[] highstNAndLowerNumber = new int[2];
-        if(x >= 100){
-            highstNAndLowerNumber[0] =  100;
-            highstNAndLowerNumber[1] =  50;
-        }else if(x >= 50){
-            highstNAndLowerNumber[0] =  50;
-            highstNAndLowerNumber[1] =  10;
-        }else if(x >= 10){
-            highstNAndLowerNumber[0] =  10;
-            highstNAndLowerNumber[1] =  5;
-        }else if(x >= 5){
-            highstNAndLowerNumber[0] =  5;
-            highstNAndLowerNumber[1] =  1;
-        }else{
-            highstNAndLowerNumber[0] =  1;
-            highstNAndLowerNumber[1] =  0;
-        }
-        return highstNAndLowerNumber;
-    }
+    public static String convertToRoman(int number) {
+        Map<Integer, String> romanMap = new LinkedHashMap<>();
+        romanMap.put(100, "C");
+        romanMap.put(90, "XC");
+        romanMap.put(50, "L");
+        romanMap.put(40, "XL");
+        romanMap.put(10, "X");
+        romanMap.put(9, "IX");
+        romanMap.put(5, "V");
+        romanMap.put(4, "IV");
+        romanMap.put(1, "I");
 
+        StringBuilder result = new StringBuilder();
 
-    private static int getRestNummber(int lowestNumber, int numberToBeConverted){
-        return numberToBeConverted % lowestNumber;
-    }
+        for (Map.Entry<Integer, String> entry : romanMap.entrySet()) {
+            int value = entry.getKey();
+            String symbol = entry.getValue();
 
-    private static String convertToRomanNumber(int number){
-        switch (number){
-            case 100: return "C";
-            case 50: return "L";
-            case 10: return "X";
-            case 5: return "V";
-            case 1: return "I";
-            default: return "Error";
+            while (number >= value) {
+                result.append(symbol);
+                number -= value;
+            }
         }
 
+        return result.toString();
     }
 }
